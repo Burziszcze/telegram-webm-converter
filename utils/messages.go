d33730e8e3eda427e7b8d24190707c7bb0fbf817
Pawel <buurzuum@gmail.com>
2024-07-06 02:44:39 +0200

    Removed unecessary functions

--- utils/messages.go
+++ utils/messages.go
@@ -25,30 +25,9 @@ func SendMessage(bot *tgbotapi.BotAPI, chatID int64, messageText string) {
 	}
 }
 
-func SendReply(bot *tgbotapi.BotAPI, update tgbotapi.Update, messageText string) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
-	msg.ReplyToMessageID = update.Message.MessageID
-	msg.ParseMode = "markdown"
-	_, err := bot.Send(msg)
-	if err != nil {
-		log.Println("Error while sending reply:", err)
-	}
-}
-
 func SendVideo(bot *tgbotapi.BotAPI, chatID int64, outputFile string) error {
 	videoMsg := tgbotapi.NewVideoUpload(chatID, outputFile)
 	_, err := bot.Send(videoMsg)
-	if err != nil {
-		log.Println("Error while sending video:", err)
-		return err
-	}
-
-	return nil
-}
-
-func SendFile(bot *tgbotapi.BotAPI, chatID int64, outputFile string) error {
-	fileMsg := tgbotapi.NewDocumentUpload(chatID, outputFile)
-	_, err := bot.Send(fileMsg)
 	if err != nil {
 		log.Println("Error while sending video:", err)
 		return err
